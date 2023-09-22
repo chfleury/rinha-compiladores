c3f8022fe39928386f287a0184ecd0b097f5338b
Christian Fleury <chfleurysiq@gmail.com>
2023-09-22 00:05:07 -0300

    wip: sum

--- rinha.js
+++ rinha.js
@@ -2,17 +2,24 @@ const ast = require("./files/print.json");
 
 const toIr = {
   Print: (node) => ["identifier", "print", toIr[node.value.kind](node.value)],
-  Str: (node) => ["literal", node.value],
+  Str: (node) => ["literal", "str", node.value],
+  Call: (node) => ["identifier", toIr[node.callee.kind](node.callee)],
+  Var:  (node) => ["identifier", node.arguments.map(arg => toIr[arg.kind](arg)), toIr[node.callee.kind](node.callee)],
+  Int: (node) => ["literal", "int", node.value]
 };
 
 const irToJs = {
     identifier: (expr) => {
-    if (expr[1] == "print") {
+    if (expr[1] === "print") {
       console.log(irToJs[expr[2][0]](expr[2]));
     }
   },
   literal: (expr) => {
-    return expr[1];
+    const literalMapper = {
+        str: (l)=> String(l),
+        int: (l) => Number(l)
+    }
+    return literalMapper[expr[1]](expr[2]);
   },
 };
 
